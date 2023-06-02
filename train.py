6bd9626f581d7fefbe31bfaf10e22b991340e13e
kavyagupta <kavya.gupta100@gmail.com>
2023-06-03 00:34:41 +0200

    fix

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import json
+from tqdm import tqdm
 from datetime import datetime
 
 import torch
@@ -6,7 +7,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 from torchmetrics.functional import dice
 
 from models.bidate_model import BiDateNet
@@ -15,30 +15,21 @@ from utils.args import parse_args
 
 
 def train_one_epoch(train_loader, net, criterion, 
-                    optimizer, epoch_index, tb_writer, device):
+                    optimizer, device):
     running_loss = 0.0
-    last_loss = 0.0
     running_score = 0.0
-    last_score = 0.0
 
-    for i, data in enumerate(train_loader):
+    for pre, post, mask in tqdm(train_loader):
         # get the inputs; data is a list of [inputs, labels]
-        inputs_pre, inputs_post, mask = (
-            data[0].to(device),
-            data[1].to(device),
-            data[2].to(device),
-        )
+        pre, post, mask = pre.to(device), post.to(device), mask.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
-        outputs = net(inputs_pre, inputs_post)
-        # outputs = torch.squeeze(outputs)
+        outputs = net(pre, post)
         mask = torch.unsqueeze(mask, 1)
         loss = criterion(outputs, mask.float())
 
         outputs = torch.sigmoid(outputs)
-        # outputs[outputs>=0.5] = 1
-        # outputs[outputs<0.5] = 0
         score = dice(outputs, mask.long())
         loss.backward()
         optimizer.step()
@@ -46,51 +37,29 @@ def train_one_epoch(train_loader, net, criterion,
         running_loss += loss.item()
         running_score += score.item()
 
-        if i % 10 == 9:
-            last_loss = running_loss / 10  # loss per batch
-            last_score = running_score / 10  # score per batch
-            print("  batch {} loss: {}".format(i + 1, last_loss))
-            print("  batch {} score: {}".format(i + 1, last_score))
-            tb_x = epoch_index * len(train_loader) + i + 1
-            tb_writer.add_scalar("Loss/train", last_loss, tb_x)
-            tb_writer.add_scalar("Loss/score", last_score, tb_x)
-            running_loss = 0.0
-            running_score = 0.0
+    return running_loss / len(train_loader), running_score / len(train_loader)
 
-    return last_loss, last_score
-
-def val(val_loader, net, criterion, tb_writer, device):
+def val(val_loader, net, criterion, device):
     net.eval()
 
-    running_vloss = 0.0
-    running_vscore = 0.0
+    running_loss = 0.0
+    running_score = 0.0
 
-    # Disable gradient computation and reduce memory consumption.
-    with torch.no_grad():
-        for i, vdata in enumerate(val_loader):
-            vinputs_pre, v_inputs_post, vmask = (
-                vdata[0].to(device),
-                vdata[1].to(device),
-                vdata[2].to(device),
-            )
-            voutputs = net(vinputs_pre, v_inputs_post)
-            vmask = torch.unsqueeze(vmask, 1)
-            vloss = criterion(voutputs, vmask.float())
-            running_vloss += vloss
+    for pre, post, mask in tqdm(val_loader):
+        # get the inputs; data is a list of [inputs, labels]
+        pre, post, mask = pre.to(device), post.to(device), mask.to(device)
 
-            voutputs = torch.sigmoid(voutputs)
-            # voutputs[voutputs>=0.5] = 1
-            # voutputs[voutputs<0.5] = 0
-            running_vscore += dice(voutputs, vmask.int())
+        outputs = net(pre, post)
+        mask = torch.unsqueeze(mask, 1)
+        loss = criterion(outputs, mask.float())
 
-    avg_vloss = running_vloss / (i + 1)
-    avg_vscore = running_vscore / (i + 1)
-   
+        outputs = torch.sigmoid(outputs)
+        score = dice(outputs, mask.long())
+        
+        running_loss += loss.item()
+        running_score += score.item()
 
-    # Log the running loss averaged per batch
-    # for both training and validation
-
-    return avg_vloss, avg_vscore
+    return running_loss / len(val_loader), running_score / len(val_loader)
 
 
 
@@ -127,9 +96,6 @@ def main():
     optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    writer = SummaryWriter("runs/chabud_trainer_{}".format(timestamp))
-    epoch_number = 0
-
     EPOCHS = 20
 
     best_vloss = 1_000_000.0
@@ -141,24 +107,13 @@ def main():
         net.train(True)
         avg_loss, avg_score = train_one_epoch(train_loader=train_loader, net=net, 
                                                 criterion=criterion, optimizer=optimizer,
-                                                epoch_index=epoch_number, tb_writer=writer, 
                                                 device=device)
 
         
         
         avg_vloss, avg_vscore = val(val_loader=val_loader, net=net, 
-                                     criterion=criterion, tb_writer=writer, 
-                                     device=device)
+                                     criterion=criterion, device=device)
         
-        writer.add_scalars(
-            "Training vs. Validation Loss",
-            {
-                "Training": avg_loss,
-                "Validation": avg_vloss,
-            },
-            epoch_number + 1,
-        )
-        writer.flush()
         print("LOSS train {} valid {}".format(avg_loss, avg_vloss))
         print("score train {} valid {}".format(avg_score, avg_vscore))
 
