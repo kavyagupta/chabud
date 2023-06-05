0f8b06ef7b4c90eac671414076c78eb12ffb1f1f
kavyagupta <kavya.gupta100@gmail.com>
2023-06-05 17:00:43 +0200

    normalize

--- utils/chabud_dataloader.py
+++ utils/chabud_dataloader.py
@@ -10,6 +10,7 @@ import rasterio as rio
 from skimage import transform
 
 import torch.utils.data as data
+from torchvision import transforms
 
 
 class ChabudDataset(data.Dataset):
@@ -46,3 +47,46 @@ class ChabudDataset(data.Dataset):
         img_mask = cv2.resize(img_mask, self.window, cv2.INTER_NEAREST)
 
         return img_pre_resize, img_post_resize, img_mask
+    
+
+def batch_mean_and_sd(loader):
+    
+    cnt = 0
+    n_channels = 12
+    fst_moment = torch.empty(n_channels)
+    snd_moment = torch.empty(n_channels)
+
+    for images, _ in loader:
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, range(2,13)])
+        sum_of_square = torch.sum(images ** 2,
+                                  dim=[0, range(2,13)])
+        fst_moment = (cnt * fst_moment + sum_) / (
+                      cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (
+                            cnt + nb_pixels)
+        cnt += nb_pixels
+
+    mean, std = fst_moment, torch.sqrt(
+      snd_moment - fst_moment ** 2)        
+    return mean, std
+
+
+f = open(f"{'../data'}/{'vectors/Original_Split-20230524T135331/MASK'}/metadata.json")
+data = json.load(f)
+train_list = data["dataset"]["train"]
+val_list = data["dataset"]["val"]
+
+chabud_val = ChabudDataset(
+        data_root='./data',
+        json_dir="vectors/Original_Split-20230524T135331/MASK",
+        data_list=val_list,
+        window=512
+    )
+
+val_loader = DataLoader(chabud_val, batch_size=4, 
+                            shuffle=False)
+  
+mean, std = batch_mean_and_sd(val_loader)
+print("mean and std: \n", mean, std)
