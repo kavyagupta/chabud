44ed5a52414406f5df381f53e1cba6e7584f5548
kavyagupta <kavya.gupta100@gmail.com>
2023-06-08 23:00:59 +0200

    fix

--- utils/chabud_dataloader.py
+++ utils/chabud_dataloader.py
@@ -42,9 +42,7 @@ class ChabudDataset(data.Dataset):
             transformed = self.transform(image = img_pre.transpose(2, 1, 0), image1 = img_post.transpose(2, 1, 0), 
                                          mask= img_mask)
             img_pre = transformed['image']
-            img_pre = img_pre.transpose(0,1,2)
             img_post = transformed['image1']
-            img_post = img_post.transpose(0,1,2)
             img_mask = transformed['mask']
         
 
@@ -56,7 +54,6 @@ class ChabudDataset(data.Dataset):
             img_pre_resize.append(cv2.resize(img_pre[i], self.window))
             img_post_resize.append(cv2.resize(img_post[i], self.window))
         
-        print(img_pre_resize.shape)
         img_pre_resize = np.asarray(img_pre_resize, dtype=np.float32)
         img_post_resize = np.asarray(img_post_resize, dtype=np.float32)
         img_mask = cv2.resize(img_mask, self.window, cv2.INTER_NEAREST)
