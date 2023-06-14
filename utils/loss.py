63523dd7c6e3b22ea1d50d941da2a41d62d61051
sagarverma <sagar15056@iiitd.ac.in>
2023-06-14 10:36:32 +0200

    jaccard loss

--- utils/loss.py
+++ utils/loss.py
@@ -109,6 +109,44 @@ def BCE(inputs, targets):
     bce = F.binary_cross_entropy(inputs, targets)
     return bce
 
+def jaccard_loss(logits, true, eps=1e-7):
+    """Computes the Jaccard loss, a.k.a the IoU loss.
+
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the jaccard loss so we
+    return the negated jaccard loss.
+
+    Args:
+        true: a tensor of shape [B, H, W] or [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+
+    Returns:
+        jacc_loss: the Jaccard loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    union = cardinality - intersection
+    jacc_loss = (intersection / (union + eps)).mean()
+    return (1 - jacc_loss)
+
 
 def get_loss(args, device):
     if args.loss == "ce":
@@ -120,6 +158,8 @@ def get_loss(args, device):
         criterion = nn.MSELoss()
     elif args.loss == 'bce_dice':
         criterion = BCEDiceLoss
+    elif args.loss == 'jaccard':
+        criterion = jaccard_loss
     else:
         print("Loss not found")
         return
