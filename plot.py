59ba7049f4863ee1dc8106bd2ac8a39274ba67cd
kavyagupta <kavya.gupta100@gmail.com>
2023-06-14 14:49:28 +0200

    plot

--- plot.py
+++ plot.py
@@ -3,7 +3,11 @@ import argparse
 import torch
 import os
 
+from utils.chabud_dataloader import get_dataloader
+from utils.loss import get_loss
+
 from models import get_model
+
 
 try:
     from engine.libs.hub import get_last_weight, get_best_weight
@@ -11,11 +15,13 @@ except ImportError:
     raise ImportError(
         'Please run "pip install granular-engine" to install engine')
 
-def weight_and_experiment(url, best=False):
+def weight_and_experiment(args):
+    net = get_model(args)
+
     if best:
-        checkpoint, experiment_id = get_best_weight(url)
+        checkpoint, experiment_id = get_best_weight(args.cpt_url)
     else:
-        checkpoint, experiment_id = get_last_weight(url)
+        checkpoint, experiment_id = get_last_weight(args.cpt_url)
 
     dst_path = 'pretrain/' + '/'.join(checkpoint.replace('gs://', '').replace('s3://', '').split('/')[2:])
     os.system(f"gsutil -m cp -n -r {checkpoint} {dst_path} 2> /dev/null")
@@ -24,12 +30,33 @@ def weight_and_experiment(url, best=False):
     if 'state_dict' in weight:
         weight = weight['state_dict']
     model_dict = net.state_dict()
-    pretrained_dict = {k: v for k, v in weight.items() if k in model_dict and v.shape == model_dict[k].shape}
-    model_dict.update(pretrained_dict)
-        
-            
+               
     return net.load_state_dict(model_dict)
 
+
+def val(val_loader, net, criterion, device):
+    # net.eval()
+
+    running_loss = 0.0
+    running_score = 0.0
+    running_iou = 0.0
+
+    for pre, post, mask in tqdm(val_loader):
+        # get the inputs; data is a list of [inputs, labels]
+        pre, post, mask = pre.to(device), post.to(device), mask.to(device)
+
+        outputs = net(pre, post)
+        loss = criterion(outputs, mask.long())
+     
+        outputs = torch.argmax(outputs, axis=1)
+        score = dice(outputs, mask)
+        iou = multiclass_jaccard_index(outputs, mask, num_classes=2)
+        
+        running_loss += loss.item()
+        running_score += score.item()
+        running_iou += iou.item()
+
+    return running_loss, running_score, running_iou
 
 
 if __name__ == '__main__':
@@ -37,13 +64,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plotting images")
 
     parser.add_argument(
-        "--weight-file", type=str, required=True, help="weight file")
+        "--cpt-url", type=str, required=True, help="url of the model")
 
     parser.add_argument(
         "--data-path", type=str, required=True, help="folder to save the images")
 
     parser.add_argument(
         "--arch", type=str, required=True, help="model arch")
+    
+    parser.add_argument(
+        "--loss", required=True, type=str, help="cross entropy/focal")
 
     # parser.add_argument(
     #     "--csv-name", type=str, required=True, help="prediction csv name")
@@ -52,6 +82,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     device = torch.device("cuda:0")
+    net = weight_and_experiment(args)
+    net.to(device)
+    net.eval()
 
-    model = get_model(args)
-    model.to(device)
+    _, val_loader = get_dataloader(args)
+
+    criterion = get_loss(args, device)
+
+    
+    vloss, vscore, viou = val(val_loader=val_loader, net=net, 
+                                        criterion=criterion, device=device)
+
