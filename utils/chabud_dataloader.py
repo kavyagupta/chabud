20aeb943eafbb0d9b6fd62494878b2d905e8387f
sagarverma <sagar15056@iiitd.ac.in>
2023-06-14 10:20:33 +0200

    fix

--- utils/chabud_dataloader.py
+++ utils/chabud_dataloader.py
@@ -137,7 +137,7 @@ def get_dataloader(args):
                                 ], p=1),
                                 A.HorizontalFlip(p=0.5), 
                                 A.VerticalFlip(p=0.5),
-                                A.RandomBrightnessContrast(p=0.2), 
+                                # A.RandomBrightnessContrast(p=0.2), 
                                 A.OneOf([
                                     A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
                                     A.GridDistortion(p=0.5),
