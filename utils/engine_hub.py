984ebe3f36aa4f61b28975e979cc263371b6931c
kavyagupta <kavya.gupta100@gmail.com>
2023-06-14 18:03:48 +0200

    fix

--- utils/engine_hub.py
+++ utils/engine_hub.py
@@ -14,10 +14,10 @@ def weight_and_experiment(url, best=False):
         checkpoint, experiment_id = get_last_weight(url)
 
     dst_path = 'pretrain/' + '/'.join(checkpoint.replace('gs://', '').replace('s3://', '').split('/')[2:])
-    os.system(f"gsutil -m cp -n -r {checkpoint} {dst_path} 2> /dev/null")
+    os.system(f"gsutil -m cp -n -r {checkpoint} {dst_path}")
 
     experiment_path = 'gs://' + '/'.join(checkpoint.replace('gs://', '').replace('s3://', '').split('/')[2:-1])
     experiment_config_path = 'pretrain/' + '/'.join(checkpoint.replace('gs://', '').replace('s3://', '').split('/')[2:-1])
-    os.system(f"gsutil -m cp -n -r {experiment_path} {experiment_config_path} 2> /dev/null")
+    os.system(f"gsutil -m cp -n -r {experiment_path} {experiment_config_path}")
             
     return dst_path, experiment_id
