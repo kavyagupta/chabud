1c05c6e1178344fce29d1ee7b8ca50afb293f4e0
sagarverma <sagar15056@iiitd.ac.in>
2023-06-16 22:00:40 +0200

    medium aug

--- utils/chabud_dataloader.py
+++ utils/chabud_dataloader.py
@@ -159,11 +159,11 @@ def get_dataloader(args):
                                 A.HorizontalFlip(p=0.5), 
                                 A.VerticalFlip(p=0.5),
                                 A.RandomBrightnessContrast(p=0.3), 
-                                # A.OneOf([
-                                #     A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
-                                #     A.GridDistortion(p=0.5),
-                                #     A.OpticalDistortion(distort_limit=1, shift_limit=0.5, p=1),
-                                #     ], p=0.8),
+                                A.OneOf([
+                                    A.ElasticTransform(p=0.5, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+                                    A.GridDistortion(p=0.5),
+                                    A.OpticalDistortion(distort_limit=1, shift_limit=0.5, p=1),
+                                    ], p=0.8),
                                 A.Resize(args.window, args.window)
                               ] + pipeline,
                               additional_targets={'post': 'image'})
